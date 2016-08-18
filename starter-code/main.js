bd7d1a59a635f28a2b7b8d94ad39f5e7dfe3fe06
Will McBride <Will@Wills-MacBook-Pro.local>
2016-08-18 16:54:15 -0400

    Adding html, css, and js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,8 @@
 console.log("JS file is connected to HTML! Woo!")
+
+// Establishing values for our cards
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
+
