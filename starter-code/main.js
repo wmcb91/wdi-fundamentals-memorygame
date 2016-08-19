9a09146bed2b7d480c7817382e59f7091108a8f9
wmcb91 <wmcb91@gmail.com>
2016-08-19 15:55:00 -0400

    updated js - chapter 9 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,3 +6,44 @@ var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
+if (cardTwo === cardFour) {
+	alert('You found a match!');
+}
+	else {
+		alert('Sorry, try again.');
+}
+
+if (cardTwo === cardOne) {
+	alert('You found a match!');
+}
+	else {
+		alert('Sorry, try again.');
+}
+
+if (cardTwo === cardThree) {
+	alert('You found a match!');
+}
+	else {
+		alert('Sorry, try again.');
+}
+
+if (cardOne === cardThree) {
+	alert('You found a match!');
+}
+	else {
+		alert('Sorry, try again.');
+}
+
+if (cardOne === cardFour) {
+	alert('You found a match!');
+}
+	else {
+		alert('Sorry, try again.');
+}
+
+if (cardThree === cardFour) {
+	alert('You found a match!');
+}
+	else {
+		alert('Sorry, try again.');
+}
