8613376f5c0258dc8f57394cbac1a2f89ffe2350
wmcb91 <wmcb91@gmail.com>
2016-08-22 01:58:06 -0400

    unit 11 assignment, not working perfectly

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,66 +1,45 @@
 console.log("JS file is connected to HTML! Woo!")
 
-// Establishing values for our cards
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+// Establishing array for the cards
+var cards = ['queen', 'queen', 'king', 'king'];
+
+//Declaring cardsInPlay
+var cardsInPlay = [];
 
 // setting the number of cards for java to create
 var numOfCards = 4;
 
 //creating function
 var createBoard = function (){
-for (var i = 1; i<'numOfCards'; i +=1)
+for (var i = 0; i<cards.length; i++)
 	var newCard = document.createElement('div');
 	newCard.classname = 'card';
+	newCard.setAttribute('data-card',cards[i]);
+	newCard.addEventListener('click', isTwoCards);
 	document.getElementbyId('game-board')[0].appendChild(newCard);
 }
 
+// creating function to determine if two cards have been picked
+var isTwoCards = function (){
+	cardsInPlay.push(this.getAttribute('data-card'));
+		console.log(this.getAttribute('data-card'));
+		if (this.getAttribute('data-card') === 'king') {
+			this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>"; 
+		} else {
+			this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>"
+		}
+		if (cardsInPlay.length === 2) {
+			isMatch(cardsInPlay);
+		cardsInPlay = [];
+		}
+}
 
 
-
-
-
-/* if (cardTwo === cardFour) {
+var isMatch = function (){
+	if (cards[0] === cards[1]) {
 	alert('You found a match!');
 }
 	else {
-		alert('Sorry, try again.');
+	alert('Sorry, try again.');
 }
-
-if (cardTwo === cardOne) {
-	alert('You found a match!');
 }
-	else {
-		alert('Sorry, try again.');
-}
-
-if (cardTwo === cardThree) {
-	alert('You found a match!');
-}
-	else {
-		alert('Sorry, try again.');
-}
-
-if (cardOne === cardThree) {
-	alert('You found a match!');
-}
-	else {
-		alert('Sorry, try again.');
-}
-
-if (cardOne === cardFour) {
-	alert('You found a match!');
-}
-	else {
-		alert('Sorry, try again.');
-}
-
-if (cardThree === cardFour) {
-	alert('You found a match!');
-}
-	else {
-		alert('Sorry, try again.');
-}
-*/
